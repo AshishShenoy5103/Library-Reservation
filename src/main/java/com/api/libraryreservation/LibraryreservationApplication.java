0a8b7398e328fe8d5839e8558cd599eee66c972b
AshishShenoy5103 <ashishshenoy510@gmail.com>
2026-07-02 11:19:19 +0530

    initial commit: library reservation project

---
+++ src/main/java/com/api/libraryreservation/LibraryreservationApplication.java
@@ -0,0 +1,13 @@
+package com.api.libraryreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryreservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryreservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/libraryreservation/LibraryreservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.libraryreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryreservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
